b59bc731e2e04e346ab654daa0eceddbb73e39bb
Mickie <mickie@gmail.com>
2022-03-29 11:37:13 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const reviewRoutes = require("./routes/reviews");
 const mongoSanitize = require("express-mongo-sanitize");
 
 // const dbUrl = process.env.DB_URL;
-const dbUrl =
-  process.env.DB_URL || "mongodb://localhost:27017/CampgroundProject";
+const dbUrl = "mongodb://localhost:27017/CampgroundProject";
+// process.env.DB_URL || "mongodb://localhost:27017/CampgroundProject";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
